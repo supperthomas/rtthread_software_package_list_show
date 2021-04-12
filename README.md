b0c61035ab20e11ddf873d5ef892ebbcef93e622
supperthomas <78900636@qq.com>
2021-04-12 23:03:19 +0800

    add first version

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # rtthread_software_package_list_show
+
+## 简介
+
+这个是用来生成RTTHREAD官方的软件包的一个小脚本，生成的样式参考   [rtthread_softlist](rtthread_softlist.md)
+
+## 用法
+
+先用命令把官方packages下下来
+
+```
+git submodule update
+```
+
+
+
+用法很简单，安装python3.X版本，在命令行里面输入命令
+
+```
+python update_softpackage.py
+```
+
+即可更新软件包
+
+## PR
+
+这个是个初期版本，欢迎PR修改。
+
+后续可以提供多个样式版本，方便生成不同的head头标题。
+
+
+
---
+++ update_softpackage.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+
+#存放的数据列表
+data_list = []
+group_name = []
+curren_name = []
+package_name = []
+github_link = []
+package_description =[]
+package_json_name = []
+package_license =[]
+package_cata = []
+package_auth = []
+
+package_list = ['iot', 'ai', 'language','misc','multimedia','peripherals', 'security', 'system','tools']
+
+filename = 'rtthread_softlist.md'
+with open(filename, 'w', encoding='utf-8') as file_object:
+    file_object.write("# RTTHREAD 软件包目录\r\n")
+    for root, dirs, files in os.walk("packages"):
+        for f in files:
+            curren_name = os.path.basename(os.path.abspath(os.path.join(root,"..")))
+            if os.path.splitext(f)[1] == '.json':
+                if group_name != curren_name:
+                    if curren_name in  package_list:
+                        group_name = curren_name
+                        file_object.write("\r\n")
+                        file_object.write("## " + group_name + "\n")
+                        file_object.write("\r\n | 包名| 作者  | license   | 备注      |"+"\n" + "| ------------------------------------------------------------ | ------------------- | -------------------- | ------------------------------------------------------------ |" + "\n")
+                package_name = os.path.basename(os.path.join(root))
+                #print(os.path.basename(os.path.join(root)))      ##package name 
+                json_path = os.path.join(root, f)
+                with  open(json_path,'r', encoding='utf-8') as json_file:
+                    json_dict = json.load(json_file, encoding="utf-8")
+                    for dict in json_dict.items():
+                        if dict[0] == "description_zh":
+                            #json_list.append(dict[1])
+                            #print(dict[1])
+                            package_description = dict[1]
+                        if dict[0] == "author":
+                            for dict_sub in dict[1].items():
+                            #json_list.append(dict[1])
+                                if dict_sub[0] == "name":
+                                    package_json_name = dict_sub[1]
+                                    #print(dict_sub)
+                            
+                        if dict[0] == "license":
+                            #json_list.append(dict[1])
+                            #print(dict[1])
+                            package_license = dict[1]
+                        if dict[0] == "repository":
+                            #print(dict[1])
+                            github_link = dict[1]
+                            #json_list.append(dict[1])
+                file_object.write("| ["+package_name + "](" +github_link + ") |"+package_json_name+"  | "+package_license+" |"+package_description + "|\n")
